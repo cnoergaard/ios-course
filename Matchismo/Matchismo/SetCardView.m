38ce3d7fe4176f14bef21ddfe02113678eec5661
Claus Nørgaard <cn@ccieurope.com>
2013-05-23 08:27:19 +0200

    SetCardView added

---
+++ Matchismo/Matchismo/SetCardView.m
@@ -0,0 +1,202 @@
+//
+//  SetCardView.m
+//  Matchismo
+//
+//  Created by Claus Noergaard on 5/23/13.
+//  Copyright (c) 2013 cn. All rights reserved.
+//
+
+#import "SetCardView.h"
+
+@implementation SetCardView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setup];
+        return self;
+    }
+    return self;
+}
+
+#pragma mark - Properties
+
+
+#define DEFAULT_FACE_CARD_SCALE_FACTOR 0.90
+
+- (void)setNumber:(NSString *)number
+{
+    _number = number;
+    [self setNeedsDisplay];
+}
+
+- (void)setSymbol:(NSString *)symbol
+{
+    _symbol = symbol;
+    [self setNeedsDisplay];
+}
+
+- (void)setShading:(NSString *)shading
+{
+    _shading = shading;
+    [self setNeedsDisplay];
+}
+
+- (void)setColor:(NSString *)color
+{
+    _color = color;
+    [self setNeedsDisplay];
+}
+
+- (void)setFaceUp:(BOOL)faceUp
+{
+    _faceUp = faceUp;
+    [self setNeedsDisplay];
+}
+
+#pragma mark - Drawing
+
+#define CORNER_RADIUS 12.0
+
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+    UIBezierPath *roundedRect = [UIBezierPath bezierPathWithRoundedRect:self.bounds cornerRadius:CORNER_RADIUS];
+    
+    [roundedRect addClip];
+    
+    [[UIColor whiteColor] setFill];
+    UIRectFill(self.bounds);
+    
+    if (self.faceUp) {
+        [self drawPips];
+        [self drawCorners];
+    } else {
+        [[UIImage imageNamed:@"cardback.png"] drawInRect:self.bounds];
+    }
+    
+    [[UIColor blackColor] setStroke];
+    [roundedRect stroke];
+}
+
+#define PIP_FONT_SCALE_FACTOR 0.20
+#define CORNER_OFFSET 2.0
+
+- (void)drawCorners
+{
+    NSMutableParagraphStyle *paragraphStyle = [[NSMutableParagraphStyle alloc] init];
+    paragraphStyle.alignment = NSTextAlignmentCenter;
+    
+    UIFont *cornerFont = [UIFont systemFontOfSize:self.bounds.size.width * PIP_FONT_SCALE_FACTOR];
+    
+    NSAttributedString *cornerText = [[NSAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@\n%@%@\n%@", self.symbol, self.shading, self.color, self.number] attributes:@{ NSParagraphStyleAttributeName : paragraphStyle, NSFontAttributeName : cornerFont }];
+    
+    CGRect textBounds;
+    textBounds.origin = CGPointMake(CORNER_OFFSET, CORNER_OFFSET);
+    textBounds.size = [cornerText size];
+    [cornerText drawInRect:textBounds];
+    
+    [self pushContextAndRotateUpsideDown];
+    [cornerText drawInRect:textBounds];
+    [self popContext];
+}
+
+- (void)pushContextAndRotateUpsideDown
+{
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSaveGState(context);
+    CGContextTranslateCTM(context, self.bounds.size.width, self.bounds.size.height);
+    CGContextRotateCTM(context, M_PI);
+}
+
+- (void)popContext
+{
+    CGContextRestoreGState(UIGraphicsGetCurrentContext());
+}
+
+#pragma mark - Draw Pips
+
+#define PIP_HOFFSET_PERCENTAGE 0.165
+#define PIP_VOFFSET1_PERCENTAGE 0.090
+#define PIP_VOFFSET2_PERCENTAGE 0.175
+#define PIP_VOFFSET3_PERCENTAGE 0.270
+
+- (void)drawPips
+{
+/*    if ((self.rank == 1) || (self.rank == 5) || (self.rank == 9) || (self.rank == 3)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 6) || (self.rank == 7) || (self.rank == 8)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:0
+                        mirroredVertically:NO];
+    }
+    if ((self.rank == 2) || (self.rank == 3) || (self.rank == 7) || (self.rank == 8) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:0
+                            verticalOffset:PIP_VOFFSET2_PERCENTAGE
+                        mirroredVertically:(self.rank != 7)];
+    }
+    if ((self.rank == 4) || (self.rank == 5) || (self.rank == 6) || (self.rank == 7) || (self.rank == 8) || (self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET3_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+    if ((self.rank == 9) || (self.rank == 10)) {
+        [self drawPipsWithHorizontalOffset:PIP_HOFFSET_PERCENTAGE
+                            verticalOffset:PIP_VOFFSET1_PERCENTAGE
+                        mirroredVertically:YES];
+    }
+*/
+ }
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                          upsideDown:(BOOL)upsideDown
+{
+    if (upsideDown) [self pushContextAndRotateUpsideDown];
+    CGPoint middle = CGPointMake(self.bounds.size.width/2, self.bounds.size.height/2);
+    UIFont *pipFont = [UIFont systemFontOfSize:self.bounds.size.width * PIP_FONT_SCALE_FACTOR];
+    NSAttributedString *attributedSuit = [[NSAttributedString alloc] initWithString:self.symbol attributes:@{ NSFontAttributeName : pipFont }];
+    
+    CGSize pipSize = [attributedSuit size];
+    CGPoint pipOrigin = CGPointMake(
+                                    middle.x-pipSize.width/2.0-hoffset*self.bounds.size.width,
+                                    middle.y-pipSize.height/2.0-voffset*self.bounds.size.height
+                                    );
+    [attributedSuit drawAtPoint:pipOrigin];
+    if (hoffset) {
+        pipOrigin.x += hoffset*2.0*self.bounds.size.width;
+        [attributedSuit drawAtPoint:pipOrigin];
+    }
+    if (upsideDown) [self popContext];
+}
+
+- (void)drawPipsWithHorizontalOffset:(CGFloat)hoffset
+                      verticalOffset:(CGFloat)voffset
+                  mirroredVertically:(BOOL)mirroredVertically
+{
+    [self drawPipsWithHorizontalOffset:hoffset
+                        verticalOffset:voffset
+                            upsideDown:NO];
+    if (mirroredVertically) {
+        [self drawPipsWithHorizontalOffset:hoffset
+                            verticalOffset:voffset
+                                upsideDown:YES];
+    }
+}
+
+#pragma mark - Initialization
+
+- (void)setup
+{
+    // do initialization here
+}
+
+- (void)awakeFromNib
+{
+    [self setup];
+}
+@end
