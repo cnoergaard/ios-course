be40c4e805010dc998495e35e4477065567b3fbe
Claus Nørgaard <cn@ccieurope.com>
2013-05-30 08:19:08 +0200

    HW4 start

---
+++ SPoT/SPoT/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SPoT
+//
+//  Created by Claus Noergaard on 5/30/13.
+//  Copyright (c) 2013 Claus Noergaard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SPoT/SPoT/SPotTVC.h
@@ -0,0 +1,13 @@
+//
+//  SPoTTVC.h
+//  SPoT
+//
+//  Created by Claus Noergaard on 5/30/13.
+//  Copyright (c) 2013 Claus Noergaard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPoTTVC : UITableViewController
+
+@end
